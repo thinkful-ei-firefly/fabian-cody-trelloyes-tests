276a22196fecdd8c4132032c64b213f5cd63df1d
krill <lpmaze@gmail.com>
2019-07-30 11:52:14 -0400

    added Snapshot testing to Card

--- src/Card.test.js
+++ src/Card.test.js
@@ -14,7 +14,15 @@ describe('<Card />', () =>{
 
   it('Renders card UI as expected', () => {
     const tree = renderer.create(<Card />).toJSON();
-    expect(tree).toMatchSnapShot();
-  })
+    expect(tree).toMatchSnapshot();
+  });
+
+//   it('renders this UI as expected', () => {
+//     // Render the component, as JSON
+//     const tree = renderer.create(<Card />).toJSON();
+//     // Check whether it matches the previous snapshot
+//     // Stored in __snapshots__/App.test.js.snap
+//     expect(tree).toMatchSnapshot(); 
+// });
 
 })
